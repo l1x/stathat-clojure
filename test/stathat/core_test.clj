e66c21ff916c8bc4706f28fda9ad0f243a82e1d3
Istvan <leccine@gmail.com>
2013-04-29 20:00:02 -0700

    Change mail

--- test/stathat/core_test.clj
+++ test/stathat/core_test.clj
@@ -3,9 +3,12 @@
             [clojure.test :refer :all]
             [stathat.core :refer :all]))
 
+;it should check if the ezkey is registered and fail if not
+;http://www.stathat.com/docs/api
+
 (deftest returns-200-on-post
   (testing "Testing is 200 is returned"
-    (is (= 200 (:status (http/post (str "http://api.stathat.com/" "ez") {:ezkey "leccine@gmail.com", 
-                                                                         :stat  "Conus geographus", 
+    (is (= 200 (:status (http/post (str "http://api.stathat.com/" "ez") {:ezkey "your@mail.com", 
+                                                                         :stat  "my lucky number", 
                                                                          :count 13}))))))
 
